4cad6cc1bfb3500ebc61a059d7fef48197de64b6
Paszkoo <japaszkopl@gmail.com>
2024-11-11 05:21:55 +0100

    Word_level_transribtion__NodeMCUv3_receives_messages

---
+++ NodeMCUv3/nodeMCUv3_temp_mqtt.ino
@@ -0,0 +1,169 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <vector>
+#include <time.h>
+
+// Zmienne dla sensora temperatury
+#define ONE_WIRE_BUS D3
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); 
+
+// WiFi i MQTT
+const char* ssid = "PaszkoIP"; // wifi name
+const char* password = "ipwifi1357"; // wifi pass
+const char* mqtt_server = "a9aa313a.ala.eu-central-1.emqxsl.com"; // mqtt broker server address
+const int mqtt_port = 8883;
+const char* mqtt_user = "testowy";
+const char* mqtt_password = "test";
+const char* topic_temperature = "temperature"; // nowy temat do wysylania temperatury
+const char* topic_whisper = "whisper"; // temat do odbierania wiadomosci
+
+// Inicjalizacja klienta z certyfikatem
+BearSSL::WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+// Certyfikat SSL brokera MQTT
+static const char ca_cert[] PROGMEM = R"EOF(-----BEGIN CERTIFICATE-----
+MIIDrzCCApegAwIBAgIQCDvgVpBCRrGhdWrJWZHHSjANBgkqhkiG9w0BAQUFADBh
+MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3
+d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD
+QTAeFw0wNjExMTAwMDAwMDBaFw0zMTExMTAwMDAwMDBaMGExCzAJBgNVBAYTAlVT
+MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxGTAXBgNVBAsTEHd3dy5kaWdpY2VydC5j
+b20xIDAeBgNVBAMTF0RpZ2lDZXJ0IEdsb2JhbCBSb290IENBMIIBIjANBgkqhkiG
+9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4jvhEXLeqKTTo1eqUKKPC3eQyaKl7hLOllsB
+CSDMAZOnTjC3U/dDxGkAV53ijSLdhwZAAIEJzs4bg7/fzTtxRuLWZscFs3YnFo97
+nh6Vfe63SKMI2tavegw5BmV/Sl0fvBf4q77uKNd0f3p4mVmFaG5cIzJLv07A6Fpt
+43C/dxC//AH2hdmoRBBYMql1GNXRor5H4idq9Joz+EkIYIvUX7Q6hL+hqkpMfT7P
+T19sdl6gSzeRntwi5m3OFBqOasv+zbMUZBfHWymeMr/y7vrTC0LUq7dBMtoM1O/4
+gdW7jVg/tRvoSSiicNoxBN33shbyTApOB6jtSj1etX+jkMOvJwIDAQABo2MwYTAO
+BgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAdBgNVHQ4EFgQUA95QNVbR
+TLtm8KPiGxvDl7I90VUwHwYDVR0jBBgwFoAUA95QNVbRTLtm8KPiGxvDl7I90VUw
+DQYJKoZIhvcNAQEFBQADggEBAMucN6pIExIK+t1EnE9SsPTfrgT1eXkIoyQY/Esr
+hMAtudXH/vTBH1jLuG2cenTnmCmrEbXjcKChzUyImZOMkXDiqw8cvpOp/2PV5Adg
+06O/nVsJ8dWO41P0jmP6P6fbtGbfYmbW0W5BjfIttep3Sp+dWOIrWcBAI+0tKIJF
+PnlUkiaY4IBIqDfv8NZ5YBberOgOzW6sRBc4L0na4UU+Krk2U886UAb3LujEV0ls
+YSEY1QSteDwsOoBrp+uvFRTp2InBuThs4pFsiv9kuXclVzDAGySj4dzp30d8tbQk
+CAUw7C29C79Fv1C5qfPrmAESrciIxpg0X40KPMbp1ZWVbd4=
+-----END CERTIFICATE-----)EOF";
+
+// Bufor do przechowywania odebranych slow
+std::vector<String> wordBuffer;
+int currentIndex = 0;
+long lastDisplayTime = 0;
+const long displayInterval = 1200; // Interwal wyswietlania slow na ekranie
+
+// Ustawienia NTP (do synchronizacji czasu)
+const char* ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 0;
+const int daylightOffset_sec = 3600;
+
+void setup() {
+  Serial.begin(9600);
+  connectToWiFi();
+  syncTime();
+  sensors.begin();
+  lcd.init();
+  lcd.backlight();
+  lcd.clear(); 
+  espClient.setTrustAnchors(new BearSSL::X509List(ca_cert));
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+  connectToMQTT();
+}
+
+void syncTime() {
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  Serial.print("Synchronizing time...");
+  while (time(nullptr) < 24 * 3600) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Time synchronized");
+}
+void connectToWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+}
+
+void connectToMQTT() {
+  while (!client.connected()) {
+    String client_id = "ESP8266Client-" + String(WiFi.macAddress());
+    Serial.printf("Connecting to MQTT Broker as %s...\n", client_id.c_str());
+    if (client.connect(client_id.c_str(), mqtt_user, mqtt_password)) {
+      Serial.println("Connected to MQTT broker");
+      client.subscribe(topic_whisper); // Subskrypcja tematu "whisper"
+    } else {
+      // Wypisanie szczegolowego bledu SSL
+      char error_msg[128];
+      espClient.getLastSSLError(error_msg, sizeof(error_msg));
+      Serial.print("Failed to connect to MQTT broker, SSL error: ");
+      Serial.println(error_msg);
+      delay(5000); // Odczekaj 5 sekund przed ponowna proba
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message received on topic: ");
+  Serial.print(topic);
+  Serial.print("]: ");
+  String message = "";
+  for (int i = 0; i < length; i++) {
+    message += (char)payload[i];
+  }
+  Serial.println(message);
+
+  // Dodanie slowa do bufora
+  wordBuffer.push_back(message);
+}
+
+void loop() {
+  if (!client.connected()) {
+    connectToMQTT();
+  }
+  client.loop();
+
+  // Aktualizacja i wysylanie temperatury co 5 sekund
+  static long lastTempUpdate = 0;
+  if (millis() - lastTempUpdate > 5000) {
+    lastTempUpdate = millis();
+    sensors.requestTemperatures();
+    float temperature = sensors.getTempCByIndex(0);
+
+    if (temperature != DEVICE_DISCONNECTED_C) {
+      String message = String(temperature, 1);
+      client.publish(topic_temperature, message.c_str()); // Wysylanie temperatury na temat "temperature"
+      Serial.print("Temperature sent: ");
+      Serial.println(message);
+    } else {
+      Serial.println("Failed to read temperature from DS18B20");
+    }
+  }
+
+  // Wyswietlanie kolejnych slow na LCD
+  long now = millis();
+  if (now - lastDisplayTime > displayInterval && !wordBuffer.empty()) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(wordBuffer[currentIndex]);
+
+    // Aktualizacja wskaznika wyswietlanego slowa
+    currentIndex = (currentIndex + 1) % wordBuffer.size();
+    lastDisplayTime = now;
+  }
+
+  // Usuwanie przestarzalych slow, jesli bufor staje sie zbyt duzy
+  if (wordBuffer.size() > 7) {
+    wordBuffer.erase(wordBuffer.begin()); // Usuwanie najstarszego slowa
+  }
+}
--- python_scripts/main.py
+++ python_scripts/main.py
@@ -11,16 +11,30 @@ model = WhisperModel(model_size, device="cpu", compute_type="int8")
 print("Model załadowany.")
 
 # Parametry nagrania i MQTT
-chunk_duration = 10  # Czas trwania chunków w sekundach
+chunk_duration = 5  # Czas trwania chunków w sekundach
 total_duration = 120  # Całkowity czas działania programu w sekundach
 broker_address = "a9aa313a.ala.eu-central-1.emqxsl.com"  # Adres brokera MQTT (zmień na odpowiedni adres)
-topic = "transcription_topic"  # Temat MQTT
+topic = "whisper"  # Temat MQTT
 username = "testowy"  # Użytkownik do logowania
 password = "test"  # Hasło użytkownika
-ca_certs = "./emqxsl-ca.crt"  # Ścieżka do certyfikatu SSL
+ca_certs = "./EMQXcert.crt"  # Ścieżka do certyfikatu SSL
 start_time = time.time()  # Start odliczania czasu
 
 chunk_number = 1
+
+def remove_polish_characters(text):
+    # Sprawdzenie, czy tekst zawiera tylko łacińskie znaki (podstawowy zakres Unicode)
+    if not all(' ' <= c <= 'z' or 'A' <= c <= 'Z' for c in text):
+        return "sprobuj ponownie"
+
+    # Zamiana polskich znaków na ich łacińskie odpowiedniki
+    replacements = {
+        'ą': 'a', 'ć': 'c', 'ę': 'e', 'ł': 'l', 'ń': 'n',
+        'ó': 'o', 'ś': 's', 'ż': 'z', 'ź': 'z',
+        'Ą': 'A', 'Ć': 'C', 'Ę': 'E', 'Ł': 'L', 'Ń': 'N',
+        'Ó': 'O', 'Ś': 'S', 'Ż': 'Z', 'Ź': 'Z'
+    }
+    return ''.join(replacements.get(c, c) for c in text)
 
 while (time.time() - start_time) < total_duration:
     print(f"Rozpoczynam nagrywanie chunka {chunk_number}...")
@@ -35,19 +49,22 @@ while (time.time() - start_time) < total_duration:
     save_audio_to_file(audio_data, filename)
 
     # Transkrypcja nagranego chunku
-    segments, info = model.transcribe(filename, beam_size=1)
+    segments, info = model.transcribe(filename, word_timestamps=True)
 
     # Wyświetlanie wykrytego języka i pewności
     print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
 
     # Wysyłanie segmentów transkrypcji jako wiadomości MQTT
     for segment in segments:
-        message = "[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text)
-        print("#################################################")
-        print(message)
-        
-        # Wysyłanie wiadomości MQTT z SSL i uwierzytelnianiem
-        send_mqtt_message(broker_address, topic, message, username, password, ca_certs)
+        for word in segment.words:
+            #message = "[%.2fs -> %.2fs] %s" % (word.start, word.end, word.word)
+            message = word.word
+            print("#################################################")
+            print(message)
+                # usuwanie polskich znakow
+            message = remove_polish_characters(message)
+                # Wysyłanie wiadomości MQTT z SSL i uwierzytelnianiem
+            send_mqtt_message(broker_address, topic, message, username, password, ca_certs)
 
     chunk_number += 1
 
